7a6b99a87800c4559221694368c872b28c100f44
Alexander Bukley <alexander_bukley@epam.com>
2016-07-06 10:37:08 +0300

    Update response processing

--- index.js
+++ index.js
@@ -9,7 +9,8 @@
     var _ = require('underscore');
     var WError = require('verror').WError;
 
-    var BASE_URL = 'https://build.phonegap.com/api/v1/';
+    var BASE_URL = 'https://build.phonegap.com';
+    var BASE_API_URL = BASE_URL + '/api/v1/';
 
     var writeIntegrationFile = function (options, callback) {
         var integrationScript;
@@ -47,22 +48,30 @@
     ];
 
     exports.getKeys = function (platform, accessToken, callback) {
-        var url = BASE_URL + 'keys' + (platform ? '/' + platform : '') + '?access_token=' + accessToken;
+        var url = BASE_API_URL + 'keys' + (platform ? '/' + platform : '') + '?access_token=' + accessToken;
 
         request.get(url, function (error, response) {
-            var unexpectedResponseError;
+            var requestError;
 
             if (error) {
                 callback(error);
                 return;
             }
 
-            if (response.statusCode === 200) {
-                callback(null, JSON.parse(response.body).keys);
-            } else {
-                unexpectedResponseError = new WError('Unexpected response from build.phonegap.com.');
-                unexpectedResponseError.code = response.statusCode;
-                callback(unexpectedResponseError);
+            switch (response.statusCode) {
+                case 200:
+                    callback(null, JSON.parse(response.body).keys);
+                    break;
+                case 401:
+                    requestError = new WError('Invalid token.');
+                    requestError.code = 401;
+                    callback(requestError);
+                    break;
+                default:
+                    requestError = new WError('Unexpected response from build.phonegap.com');
+                    requestError.code = 500;
+                    callback(requestError);
+                    break;
             }
         });
     };
@@ -70,7 +79,7 @@
     exports.registerPhonegapApp = function (name, accessToken, packagePath, keys, callback) {
         var form, req;
 
-        req = request.post(BASE_URL + 'apps?access_token=' + accessToken, function (error, response) {
+        req = request.post(BASE_API_URL + 'apps?access_token=' + accessToken, function (error, response) {
             var body, requestError;
 
             if (error) {
@@ -79,15 +88,28 @@
             }
 
             body = JSON.parse(response.body);
-            if (response.statusCode === 201) {
-                callback(null, {
-                    id: body.id,
-                    title: body.title
-                });
-            } else {
-                requestError = new WError(body.error);
-                requestError.code = response.statusCode;
-                callback(requestError);
+            switch (response.statusCode) {
+                case 201:
+                    callback(null, {
+                        id: body.id,
+                        title: body.title
+                    });
+                    break;
+                case 400:
+                    requestError = new WError(body.error);
+                    requestError.code = 400;
+                    callback(requestError);
+                    break;
+                case 401:
+                    requestError = new WError('Invalid token.');
+                    requestError.code = 401;
+                    callback(requestError);
+                    break;
+                default:
+                    requestError = new WError('Unexpected response from build.phonegap.com');
+                    requestError.code = 500;
+                    callback(requestError);
+                    break;
             }
         });
         form = req.form();
@@ -127,8 +149,8 @@
     };
 
     // platform parametr can be one from SUPPORTED_PLATFORMS or 'all'
-    exports.buildPhonegapApp = function (appId, platform, accessToken, callback) {
-        var url = BASE_URL + 'apps/' + appId + '/build';
+    exports.buildPhonegapApp = function (id, platform, accessToken, callback) {
+        var url = BASE_API_URL + 'apps/' + id + '/build';
 
         if (platform !== 'all') {
             url += '/' + platform;
@@ -136,26 +158,36 @@
         url += '?access_token=' + accessToken;
 
         request.post(url, function (error, response) {
-            var unexpectedResponseError;
+            var requestError;
 
             if (error) {
                 callback(error);
                 return;
             }
 
-            if (response.statusCode === 202) {
-                callback();
-            } else {
-                unexpectedResponseError = new WError('Unable to start build. ' +
-                    'Unexpected response from build.phonegap.com.');
-                unexpectedResponseError.code = response.statusCode;
-                callback(unexpectedResponseError);
+            switch (response.statusCode) {
+                case 202:
+                    break;
+                case 401:
+                    requestError = new WError('Invalid token.');
+                    requestError.code = 401;
+                    break;
+                case 404:
+                    requestError = new WError('Application with id: %d. Doesn\'t exist.', id);
+                    requestError.code = 404;
+                    break;
+                default:
+                    requestError = new WError('Unable to start build. Unexpected response from build.phonegap.com');
+                    requestError.code = 500;
+                    break;
             }
+
+            callback(requestError);
         });
     };
 
-    exports.updatePhonegapApp = function (appId, accessToken, packagePath, callback) {
-        request.put(BASE_URL + 'apps/' + appId + '?access_token=' + accessToken,
+    exports.updatePhonegapApp = function (id, accessToken, packagePath, callback) {
+        request.put(BASE_API_URL + 'apps/' + id + '?access_token=' + accessToken,
             function (error, response) {
                 var body, responseError;
 
@@ -169,18 +201,17 @@
                     case 200:
                         break;
                     case 401:
-                        responseError = new WError('Invalid PhoneGap Id');
+                        responseError = new WError('Invalid token.');
+                        responseError.code = 401;
                         break;
                     case 404:
-                        responseError = new WError(body.error);
+                        responseError = new WError('Application with id: %d. Doesn\'t exist.', id);
+                        responseError.code = 404;
                         break;
                     default:
                         responseError = new WError('Unexpected response from build.phonegap.com');
+                        responseError.code = 500;
                         break;
-                }
-
-                if (responseError) {
-                    responseError.code = response.statusCode;
                 }
 
                 callback(responseError);
@@ -191,7 +222,7 @@
 
     exports.getDownloadLink = function (id, platform, accessToken, callback) {
         request.get({
-            url: BASE_URL + 'apps/' + id + '/' + platform + '?access_token=' + accessToken,
+            url: BASE_API_URL + 'apps/' + id + '/' + platform + '?access_token=' + accessToken,
             followRedirect: false
         }, function (error, response) {
             var requestError;
@@ -205,8 +236,13 @@
                 case 302:
                     callback(null, JSON.parse(response.body).location);
                     break;
+                case 401:
+                    requestError = new WError('Invalid token.');
+                    requestError.code = 401;
+                    callback(requestError);
+                    break;
                 case 404:
-                    requestError = new WError('Application with id: %d. Doesn\'t exists.', id);
+                    requestError = new WError('Application with id: %d. Doesn\'t exist.', id);
                     requestError.code = 404;
                     callback(requestError);
                     break;
@@ -265,10 +301,10 @@
     };
 
     exports.isApplicationExists = function (id, accessToken, callback) {
-        var url = BASE_URL + 'apps/' + id + '?access_token=' + accessToken;
+        var url = BASE_API_URL + 'apps/' + id + '?access_token=' + accessToken;
 
         request.get(url, function (error, response) {
-            var unexpectedResponseError;
+            var requestError;
 
             if (error) {
                 callback(error);
@@ -279,20 +315,25 @@
                 case 200:
                     callback(null, true);
                     break;
+                case 401:
+                    requestError = new WError('Invalid token.');
+                    requestError.code = 401;
+                    callback(requestError);
+                    break;
                 case 404:
                     callback(null, false);
                     break;
                 default:
-                    unexpectedResponseError = new WError('Unexpected response from build.phonegap.com');
-                    unexpectedResponseError.code = 500;
-                    callback(unexpectedResponseError);
+                    requestError = new WError('Unexpected response from build.phonegap.com');
+                    requestError.code = 500;
+                    callback(requestError);
                     break;
             }
         });
     };
 
     exports.getApplicationInformation = function (id, accessToken, callback) {
-        var url = BASE_URL + 'apps/' + id + '?access_token=' + accessToken;
+        var url = BASE_API_URL + 'apps/' + id + '?access_token=' + accessToken;
 
         request.get(url, function (error, response) {
             var requestError;
@@ -306,8 +347,12 @@
                 case 200:
                     callback(null, JSON.parse(response.body));
                     break;
+                case 401:
+                    requestError = new WError('Invalid token.');
+                    requestError.code = 401;
+                    break;
                 case 404:
-                    requestError = new WError('Application with id: %d. Doesn\'t exists.', id);
+                    requestError = new WError('Application with id: %d. Doesn\'t exist.', id);
                     requestError.code = 404;
                     callback(requestError);
                     break;
@@ -318,5 +363,38 @@
                     break;
             }
         });
+    };
+
+    exports.unlockPhonegapSigningKey = function (keyLink, passwordObject, accessToken, callback) {
+        var url = BASE_URL + keyLink + '?access_token=' + accessToken;
+
+        request.put(url, function (error, response) {
+            var requestError;
+
+            if (error) {
+                callback(error);
+                return;
+            }
+
+            switch (response.statusCode) {
+                case 202:
+                    break;
+                case 401:
+                    requestError = new WError('Invalid token.');
+                    requestError.code = 401;
+                    break;
+                case 404:
+                    requestError = new WError('Key doesn\'t exist.');
+                    requestError.code = 404;
+                    break;
+                default:
+                    requestError = new WError('Unexpected response from build.phonegap.com');
+                    requestError.code = 500;
+                    break;
+            }
+            callback(requestError);
+        })
+        .form()
+        .append('data', JSON.stringify(passwordObject));
     };
 })();
